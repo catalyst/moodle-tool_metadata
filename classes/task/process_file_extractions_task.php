74d7d874831966e4e92969bb1667f5d3c12c5961
Tom Dickman <tomdickman@catalyst-au.net>
2020-02-19 15:08:07 +1100

    refactor: Move extraction processing into abstract class
    Abstracted the base logic for creating metadata extraction tasks
    away from resource specific tasks into a base class that can
    be extended for supported resource types.
    This will make it easier to implement new resource types in the
    future and avoid code duplication.

--- classes/task/process_file_extractions_task.php
+++ classes/task/process_file_extractions_task.php
@@ -24,12 +24,6 @@
 
 namespace tool_metadata\task;
 
-use stdClass;
-use core\task\scheduled_task;
-use tool_metadata\api;
-use tool_metadata\extraction;
-use tool_metadata\plugininfo\metadataextractor;
-
 defined('MOODLE_INTERNAL') || die();
 
 /**
@@ -39,12 +33,7 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright  2019 Tom Dickman <tomdickman@catalyst-au.net>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class process_file_extractions_task extends scheduled_task {
-
-    /**
-     * Maximum file extractions to process for each extractor.
-     */
-    const MAX_PROCESSES = 400;
+class process_file_extractions_task extends process_extractions_base_task {
 
     /**
      * Get a descriptive name for this task (shown to admins).
@@ -57,187 +46,36 @@ class process_file_extractions_task extends scheduled_task {
     }
 
     /**
-     * Get an array of records representing all files and their extractions to be processed.
+     * Get the resource type this process extractions task is for.
      *
-     * Note: These records are indexed by a concatenation of the id number of the file and the
-     * plugginname of the metadataextractor subplugin conducting the extraction, eg. '44tika'.
-     * If there is no extractions for a particular file id, the record is indexed by file id
-     * and a string indicating that no extractions have been conducted, eg. '33none'.
-     *
-     * @param array $extractors string[] of the extractors to use for file metadata extraction, indexed by subplugin name.
-     *
-     * @return array
+     * @return string
      */
-    public function get_file_extractions_to_process(array $extractors = []) : array {
+    public function get_resource_type() {
+        return TOOL_METADATA_RESOURCE_TYPE_FILE;
+    }
+
+    /**
+     * Get extraction condition for file resources preventing extraction of directories.
+     *
+     * @param string $tablealias the table alias being used for the resource table.
+     *
+     * @return array $conditions object[] of object instances containing:
+     *  {
+     *      'sql' => (string) The SQL statement to add to where clause.
+     *      'params => (array) Values for bound parameters in the SQL statement indexed by parameter name.
+     *  }
+     */
+    public function get_resource_extraction_conditions($tablealias) {
         global $DB;
 
-        $startid = get_config('tool_metadata', 'processfilesstartid');
+        $conditions = [];
 
-        if (!$startid) {
-            $startid = 0;
-            set_config('processfilesstartid', $startid, 'tool_metadata');
-        }
+        // Do not extract metadata from file directories.
+        $notdirectory = new \stdClass();
+        $notdirectory->sql = $DB->sql_like($tablealias . '.filename', ':directory', true, false, true);
+        $notdirectory->params = ['directory' => '.'];
+        $conditions[] = $notdirectory;
 
-        $filerecords = [];
-        foreach ($extractors as $extractor) {
-            $name = $extractor->get_name();
-
-            // Build a unique id from the file id and extractor plugin name.
-            $uniqueid = $DB->sql_concat('f.id', "'" . $name . "'");
-
-            // We use a left outer join here to capture files which don't have extractions for the extractor.
-            $sql = "SELECT $uniqueid as uniqueid, f.id as fileid, e.id as extractionid, 
-                    f.contenthash, e.extractor, e.status, e.timemodified
-                FROM {files} f
-                LEFT OUTER JOIN {metadata_extractions} e
-                    ON f.id = e.resourceid
-                    AND (e.type = :type OR e.type IS NULL)
-                    AND (e.extractor = :extractor OR e.extractor IS  NULL)
-                WHERE f.id > :startid
-                    AND f.filename <> :directory";
-
-            $params = [
-                'type' => TOOL_METADATA_RESOURCE_TYPE_FILE,
-                'extractor' => $name,
-                'startid' => $startid,
-                'directory' => '.'
-            ];
-
-            $extractorrecords = $DB->get_records_sql($sql, $params, $startid, self::MAX_PROCESSES);
-            $filerecords = array_merge($filerecords, $extractorrecords);
-        }
-
-        return $filerecords;
-    }
-
-    /**
-     * Process the file extraction records and get statuses.
-     *
-     * @param array $records the file extraction records to process.
-     * @param array $extractors string[] of the extractors to use for file metadata extraction, indexed by subplugin name.
-     *
-     * @return \stdClass $statussummary an object containing status information on file extractions processed.
-     */
-    public function process_file_extractions(array $records = [], array $extractors = []) : stdClass {
-
-        $statussummary = new stdClass();
-        $statussummary->completed = 0; // Count of successfully completed metadata extractions.
-        $statussummary->pending = 0; // Count of pending metadata extractions (those already queued but not completed.)
-        $statussummary->queued = 0; // Count of queued metadata extractions.
-        $statussummary->errors = 0; // Count of metadata extraction errors identified.
-        $statussummary->unsupported = 0; // Count of files for which extraction is not supported by a particular plugin.
-        $statussummary->unknown = 0; // Count of metadata extractions with an unknown state.
-
-        $parsedrecordhashes = [];
-        foreach ($extractors as $extractor) {
-            $parsedrecordhashes[$extractor->get_name()] = $extractor->get_extracted_resourcehashes();
-        }
-
-        $fs = get_file_storage();
-
-        foreach ($records as $record) {
-
-            $file = $fs->get_file_by_id($record->fileid);
-
-            if (!empty($record->extractor)) {
-                if (in_array($record->contenthash, $parsedrecordhashes[$record->extractor])) {
-                    // We have already parsed the extraction of this file by this extractor.
-                    continue;
-                } else {
-                    $parsedrecordhashes[$record->extractor][] = $record->contenthash;
-                }
-
-                switch ($record->status) {
-                    case extraction::STATUS_PENDING :
-                    case extraction::STATUS_ACCEPTED :
-                        if ($record->timemodified < (time() - 86400)) {
-                            // Extraction has been pending or was started over 24 hours ago,
-                            // something probably went wrong, process again.
-                            api::async_metadata_extraction($file, TOOL_METADATA_RESOURCE_TYPE_FILE, $extractors[$record->extractor]);
-                            $statussummary->queued++;
-                        } else {
-                            $statussummary->pending++;
-                        }
-                        break;
-
-                    case extraction::STATUS_NOT_FOUND :
-                        api::async_metadata_extraction($file, TOOL_METADATA_RESOURCE_TYPE_FILE, $extractors[$record->extractor]);
-                        $statussummary->queued++;
-                        break;
-
-                    case extraction::STATUS_NOT_SUPPORTED :
-                        $statussummary->unsupported++;
-                        break;
-
-                    case extraction::STATUS_ERROR :
-                        $statussummary->errors++;
-                        break;
-
-                    case extraction::STATUS_COMPLETE :
-                        $statussummary->completed++;
-                        break;
-
-                    default :
-                        $statussummary->unknown++;
-                        break;
-
-                }
-            } else {
-                // We don't have an extractions for this file for at least one extractor, so check with all enabled
-                // extractors and process if not already done.
-                foreach ($extractors as $extractor) {
-                    if (!in_array($record->contenthash, $parsedrecordhashes[$extractor->get_name()])){
-                        api::async_metadata_extraction($file, TOOL_METADATA_RESOURCE_TYPE_FILE, $extractor);
-                        $parsedrecordhashes[$extractor->get_name()][] = $record->contenthash;
-                        $statussummary->queued++;
-                    }
-                }
-            }
-        }
-
-        return $statussummary;
-    }
-
-    /**
-     * Do the job.
-     * Throw exceptions on errors (the job will be retried).
-     */
-    public function execute() {
-
-        $enabledplugins = metadataextractor::get_enabled_plugins();
-        $extractors = [];
-
-        // Filter out plugins which don't support file metadata extraction.
-        foreach ($enabledplugins as $plugin) {
-            $extractor = api::get_extractor($plugin);
-            if ($extractor->supports_resource_type(TOOL_METADATA_RESOURCE_TYPE_FILE)) {
-                $extractors[$plugin] = $extractor;
-            }
-        }
-
-        if (empty($extractors)) {
-            mtrace('tool_metadata: No enabled metadata subplugins support file extraction, file metadata processing skipped.');
-        } else {
-            $recordstoprocess = $this->get_file_extractions_to_process($extractors);
-            $processresults = $this->process_file_extractions($recordstoprocess, $extractors);
-
-            $recordcount = count($recordstoprocess);
-
-            if ($recordcount < self::MAX_PROCESSES) {
-                // We reached the end of the {files} table, start again from the beginning on next run.
-                set_config('processfilesstartid', 0, 'tool_metadata');
-            } else {
-                // Set the startid for the next task run to the last id of this run.
-                // (This may cause double processing of some extractions, but it will avoid skipping any
-                // extractions for the last file of this run.)
-                set_config('processfilesstartid', end($recordstoprocess)->fileid, 'tool_metadata');
-            }
-
-            mtrace('tool_metadata: Count of completed extractions processed = ' . $processresults->completed);
-            mtrace('tool_metadata: Count of extractions queued = ' . $processresults->queued);
-            mtrace('tool_metadata: Count of extractions not supported by a particular plugin = ' . $processresults->unsupported);
-            mtrace('tool_metadata: Count of extraction errors identified = ' . $processresults->errors);
-            mtrace('tool_metadata: Count of extractions with unknown state = ' . $processresults->unknown);
-        }
+        return $conditions;
     }
 }
